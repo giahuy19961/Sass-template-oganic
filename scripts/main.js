de533e2e2980d3c11024d08d42e6d39f625726cc
giahuy19961 <70837177+giahuy19961@users.noreply.github.com>
2021-10-07 22:03:34 +0700

    '

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,33 @@
-var sidebarEle = document.querySelector(".sidebar-mobile");
-
 var btnMobile = document.querySelector(".header-mobile");
-console.log(sidebarEle, btnMobile);
 var overlayEle = document.querySelector(".sidebar-mobile-overlay");
-function hanldeMenu() {
-  var open = sidebarEle.classList.contains("sidebar-mobile--open");
-  console.log(open);
-  if (!open) {
-    sidebarEle.classList.add("sidebar-mobile--open");
+var categoryEle = document.querySelector(".banner-menu-header");
+
+function toggleCategory() {
+  var categoryList = document.querySelector(".banner-menu-list");
+  var closed = categoryList.classList.contains("banner-menu--closed");
+
+  if (!closed) {
+    categoryList.classList.add("banner-menu--closed");
   } else {
-    sidebarEle.classList.remove("sidebar-mobile--open");
+    categoryList.classList.remove("banner-menu--closed");
   }
 }
+// Handler toggle
+function hanldeMenu() {
+  var sidebarEle = document.querySelector(".sidebar-mobile");
+  var open = sidebarEle.classList.contains("sidebar-mobile--open");
+
+  if (!open) {
+    sidebarEle.classList.add("sidebar-mobile--open");
+    document.body.style.overflowY = "hidden";
+  } else {
+    sidebarEle.classList.remove("sidebar-mobile--open");
+    document.body.style.overflowY = "visible";
+  }
+}
+
+// Toggle on off side bar
 btnMobile.addEventListener("click", () => hanldeMenu());
 overlayEle.addEventListener("click", () => hanldeMenu());
+// Toggle on off togleCategory
+categoryEle.addEventListener("click", () => toggleCategory());
